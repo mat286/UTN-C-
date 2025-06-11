4cb9ff1b4f2fd1fe5e31382301d7fe5bfb6a3119
Mateo Facundo Rivera <mfrivera@ONP.mecon.ar>
2025-06-11 17:30:58 -0300

    Funcion de ordenamiento

--- main.cpp
+++ main.cpp
@@ -21,7 +21,56 @@ bool ordenMenorMayor(int a, int b) {
     return a > b; 
     
 }
-    
+
+void ordenar(int * arr, punteroFun comparar){
+    int aux=0;
+	int i=0;
+		while(i!=14){
+		    if( comparar(arr[i], arr[i+1]) ) {
+				aux=arr[i+1];
+				arr[i+1]=arr[i];
+				arr[i]=aux;
+				i=0;
+			}else{
+			    i++;
+			}
+			
+		}
+}
+
+int main()
+{
+	int numero=0;
+	int arr[15]= {5,8,3,10,4,7,6,1,7,5,9,7,4,1,3};
+
+	cout<<"Como queres ordenarlo\n";
+	cout<<"- (0) se mayor a menor\n- (1) de menor a mayor\n";
+	cin >>numero;
+	cout << "\n";
+	
+	punteroFun comparar;
+	
+	if(numero==1){
+	    comparar = ordenMenorMayor;
+	}else if(numero==0){
+	    comparar = ordenMayorMenor;
+	}
+	//cout<<comparar;
+	
+	if(numero==0 || numero ==1){
+	    
+	    ordenar(arr, comparar);
+	    for(int i=0; i<15; i++) {
+	        cout<< arr[i]<< " ";
+	    }
+	}else{
+	    cout<<"Numero mal ingresado\n";
+	}
+	return 0;
+}
+
+
+
 /*void ordenMayorMenor1( int* arr) {
 	//cout<< *arr;
 	//int arrA[15]=arr
@@ -58,52 +107,6 @@ void ordenMenorMayor1( int* arr) {
 		}
 }
 */
-
-void ordenar(int * arr, punteroFun comparar){
-    int aux=0;
-	int i=0;
-		while(i!=14){
-		    if( comparar(arr[i], arr[i+1]) ) {
-				aux=arr[i+1];
-				arr[i+1]=arr[i];
-				arr[i]=aux;
-				i=0;
-			}else{
-			    i++;
-			}
-			
-		}
-}
-
-int main()
-{
-	int numero=0;
-	int arr[15]= {5,8,3,10,4,7,6,1,7,5,9,7,4,1,3};
-
-	cout<<"Como queres ordenarlo\n";
-	cout<<"- (0) se mayor a menor\n- (1) de menor a mayor\n";
-	cin >>numero;
-	cout << "\n";
-	punteroFun comparar;
-	
-	if(numero==1){
-	    comparar = ordenMayorMenor;
-	}else if(numero==0){
-	    comparar = ordenMenorMayor;
-	}
-	cout<<comparar;
-	
-	if(numero==0 || numero ==1){
-	    ordenar(arr, comparar);
-	}
-	
-	
-	for(int i=0; i<15; i++) {
-		cout<< arr[i]<< " ";
-	}
-	return 0;
-}
-
 
 
 
